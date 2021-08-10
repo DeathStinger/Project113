19965b8ad59dcd83257e9ad2a431f5b8479a7473
DeathStinger <87364785+DeathStinger@users.noreply.github.com>
2021-08-10 16:41:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function Preload() {
+}
+function setup() {
+   canvas= createCanvas(640,480);
+   canvas.position(150,150);
+     video= createCapture(VIDEO);
+   video.hide();
+ 
+}
+
+function draw() {
+
+  image(video,230,150,220,200);
+  fill(250,0,0);
+ stroke(250,0,0);
+ circle(50,50,80,) ;
+ 
+  fill(250,0,0);
+ stroke(250,0,0);
+ circle(600,50,80,) ;
+ 
+  fill(250,0,0);
+ stroke(250,0,0);
+ circle(50,440,80,) ;
+
+  fill(250,0,0);
+ stroke(250,0,0);
+ circle(600,440,80,) ;
+ 
+  
+}
+
+
